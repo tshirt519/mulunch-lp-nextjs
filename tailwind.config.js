e4e05d18efe42ad2778ce3027480c32166912c58
tshirt519 <tshirt519@gmail.com>
2021-08-27 17:11:02 +0900

    🎨 Topのtailwind化　responsive対応要検討

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    minHeight: {
+      '0': '0',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      'full': '100%',
+      'overScreen': '115vh',
+     },
     extend: {
       colors: {
         mulunchOrange: "#ea9044",
@@ -14,6 +22,9 @@ module.exports = {
         desktop: { max: "1280px" },
         monitor: { max: "1536px" },
       },
+      backgroundImage: theme => ({
+        'topVisual': "url('/TV-6.jpg')",
+       })
     },
   },
   variants: {
