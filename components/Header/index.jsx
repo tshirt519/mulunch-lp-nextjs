039712f3327063793664afc9e8dab4d2c4de4765
tshirt519 <tshirt519@gmail.com>
2021-08-27 15:28:19 +0900

    🎨 lpのcssをtailwindに書き換え

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,4 +1,3 @@
-import styles from "../Header/Header.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import { Link as Scroll } from "react-scroll";
@@ -7,13 +6,13 @@ import { Header as ReponsiveHeader } from "/components/Responsive/Header";
 
 export const Header = () => {
   return (
-    <section>
-      <header className={styles.header}>
-        <nav className={styles.nav}>
-          <ul className={styles.ul}>
-            <h1 className={styles.logo}>
+    <section className="flex justify-center">
+      <header className="h-24 w-11/12 bg-white opacity-80 rounded-xl fixed z-10 mx-auto top-10 shadow tablet:hidden">
+        <nav className="h-full">
+          <ul className="flex justify-around items-center w-full h-full">
+            <h1 className="mt-2">
               <Scroll to="top" smooth={true} duration={1000} offset={0}>
-                <a className={styles.a}>
+                <a className="cursor-pointer hover:opacity-40 flex">
                   <Image
                     src="/mulunch_logo.png"
                     alt="mulunch logo"
@@ -23,66 +22,68 @@ export const Header = () => {
                 </a>
               </Scroll>
             </h1>
-            <li className={styles.li}>
+            <li>
               <Scroll to="concept" smooth={true} duration={1000} offset={-50}>
-                <a className={styles.a}>CONCEPT</a>
+                <a className="cursor-pointer hover:opacity-40 flex">CONCEPT</a>
               </Scroll>
             </li>
-            <li className={styles.li}>
+            <li>
               <Scroll to="project" smooth={true} duration={1000} offset={50}>
-                <a className={styles.a}>PROJECTS</a>
+                <a className="cursor-pointer hover:opacity-40 flex">PROJECTS</a>
               </Scroll>
             </li>
-            <li className={styles.li}>
+            <li>
               <Scroll to="topic" smooth={true} duration={1000} offset={0}>
-                <a className={styles.a}>TOPICS</a>
+                <a className="cursor-pointer hover:opacity-40 flex">TOPICS</a>
               </Scroll>
             </li>
-            <li className={styles.li}>
+            <li>
               <Scroll to="village" smooth={true} duration={1000} offset={120}>
-                <a className={styles.a}>VILLAGE</a>
+                <a className="cursor-pointer hover:opacity-40 flex">VILLAGE</a>
               </Scroll>
             </li>
-            <li className={styles.li}>
+            <li>
               <Scroll to="lunch" smooth={true} duration={1000} offset={0}>
-                <a className={styles.a}>LUNCH</a>
+                <a className="cursor-pointer hover:opacity-40 flex">LUNCH</a>
               </Scroll>
             </li>
-            <li className={styles.li}>
+            <li>
               <Scroll to="gallery" smooth={true} duration={1000} offset={-600}>
-                <a className={styles.a}>GARLLERY</a>
+                <a className="cursor-pointer hover:opacity-40 flex">GARLLERY</a>
               </Scroll>
             </li>
-            <li className={styles.li}>
+            <li>
               <Scroll to="join" smooth={true} duration={1000} offset={-50}>
-                <a className={styles.a}>JOIN</a>
+                <a className="cursor-pointer hover:opacity-40 flex">JOIN</a>
               </Scroll>
             </li>
-            <li className={styles.li}>
+            <li>
               <Link href="/blog">
-                <a className={styles.join}>BLOG</a>
+                <a className="text-white bg-mulunchOrange py-2 px-10 rounded-full shadow-md hover:opacity-80">
+                  BLOG
+                </a>
               </Link>
             </li>
           </ul>
         </nav>
       </header>
 
-      <ul className={styles.sns_nav}>
-        <li className={styles.sns_link}>
+      <ul className="flex justify-end items-center h-10 w-11/12 fixed z-10 mx-auto top-36 tablet:hidden">
+        <li className="flex justify-center items-center w-10 h-10 mx-1 bg-white opacity-60 rounded-full shadow hover:opacity-80">
           <Link href="https://twitter.com/muramusubi/">
             <a>
               <IoLogoTwitter size={24} />
             </a>
           </Link>
         </li>
-        <li className={styles.sns_link}>
+        <li className="flex justify-center items-center w-10 h-10 mx-1 bg-white opacity-60 rounded-full shadow hover:opacity-80">
           <Link href="https://www.instagram.com/muramusubi/">
             <a>
               <IoLogoInstagram size={24} />
             </a>
           </Link>
         </li>
-        <li className={styles.sns_link}>
+        <li className="flex justify-center items-center w-10 h-10 mx-1 bg-white opacity-60 rounded-full shadow hover:opacity-80">
           <Link href="https://www.facebook.com/muramusubi/">
             <a>
               <IoLogoFacebook size={24} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mulunchOrange: "#ea9044",
+        // default color => #de9044
+      },
+      screens: {
+        sp: { max: "640px" },
+        tablet: { max: "768px" },
+        laptop: { max: "1024px" },
+        desktop: { max: "1280px" },
+        monitor: { max: "1536px" },
+      },
+    },
   },
   variants: {
     extend: {},
